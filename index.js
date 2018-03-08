45d902b937cf615753e6ba6a1c1eac2863224e05
Dean Hetherington <deanhet@gmail.com>
2018-03-08 13:50:02 +0000

    Update style prop types

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ const { UIManager } = NativeModules
 export default class TextMarquee extends PureComponent {
 
   static propTypes = {
-    style:           PropTypes.object,
+    style:           PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.array
+    ]),
     duration:        PropTypes.number,
     loop:            PropTypes.bool,
     bounce:          PropTypes.bool,
