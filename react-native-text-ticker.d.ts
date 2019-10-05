db7310282b63efb084acd8fbea9f8acb51db341e
Roberto Wesley Overdijk <r.w.overdijk@gmail.com>
2019-10-05 21:31:02 +0200

    initial typescript types file

---
+++ react-native-text-ticker.d.ts
@@ -0,0 +1,27 @@
+declare module 'react-native-text-ticker' {
+  import React from 'react';
+  import { StyleProp, TextProps, TextStyle } from 'react-native';
+
+  export interface TextTickerProps extends TextProps {
+    style?: StyleProp<TextStyle>;
+    duration: number;
+    loop?: boolean;
+    bounce?: boolean;
+    scroll?: boolean;
+    marqueeOnMount?: boolean;
+    marqueeDelay?: number;
+    isInteraction?: boolean;
+    useNativeDriver?: boolean;
+    onMarqueeComplete: () => void;
+    onScrollStart: () => void;
+
+    repeatSpacer?: number;
+    easing?: (value: number) => number;
+    animationType?: string;
+    scrollingSpeed?: number;
+    shouldAnimateTreshold?: number;
+  }
+
+  export default class TextTicker<T> extends React.Component<TextTickerProps> {
+  }
+}
